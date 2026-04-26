245b643379055992d835033fd46f477f25a05aa9
Min23456 <oksumilee@gmail.com>
2026-04-26 19:56:24 -0400

    21312313123

--- script.js
+++ script.js
@@ -6,11 +6,17 @@ const resultText = document.getElementById("resultText");
 const winsText = document.getElementById("wins");
 const lossesText = document.getElementById("losses");
 const coinText = document.getElementById("coinText");
+const coin = document.getElementById(coin);
+
 let wins = 0;
 let losses = 0;
-
+let isFlipping = false;
 
 function playGame(userGuess) {
+    if (isFlipping) return;
+    isFlipping = true;
+    
+    
     const randomNumber = Math.random();
     let coinResult;
     if (randomNumber < 0.5) {
@@ -19,16 +25,32 @@ function playGame(userGuess) {
         coinResult = "Tails";
     }
     guessText.textContent = userGuess;
-    coinText.textContent = coinResult;
-    if (userGuess === coinResult) {
-        resultText.textContent = "WINNER! 🏆💰 The coin landed exactly how you wanted! Let's goooo! 🚀";
-        wins++;
+    coinText.textContent = "...";
+    resultText.textContent = "Flipping... 🪙";
+
+    coin.classList.remove("flip-heads");
+    void coin.offsetWidth;
+
+    if (coinResult === "Heads") {
+        coin.classList.add(flip-heads);
     } else {
-        losses++;
-        resultText.textContent = "Mission failed! ❌ The coin betrayed you! 🪙💨"
+        coin.classList.add(flip-heads);
     }
-    winsText.textContent = wins;
-    lossesText.textContent = losses;
+
+    setTimeout(function() {
+        coinText.textContent = coinResult;
+        if (userGuess === coinResult) {
+            resultText.textContent = "WINNER! 🏆💰 The coin landed exactly how you wanted! Let's goooo! 🚀";
+            wins++;
+        } else {
+            losses++;
+            resultText.textContent = "Mission failed! ❌ The coin betrayed you! 🪙💨"
+        }
+        winsText.textContent = wins;
+        lossesText.textContent = losses;
+        isFlipping = false;
+        }, 1500);
+
 }
 
 headsBtn.addEventListener("click", function() {
@@ -55,6 +77,7 @@ resetBtn.addEventListener("click", function() {
     guessText.textContent = "-";
     coinText.textContent = "-";
     resultText.textContent = "The coin is in the air... 🪙💨 Quick! Call it! Heads 👱‍♂️ or Tails 🦅?"
+    coin.classList.remove("flip-heads", "flip-tails");
 
 
 });
